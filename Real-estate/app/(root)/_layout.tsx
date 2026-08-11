8f305969692c5386fbe12e9b982930f86ea14324
Ikilia Doreen <ikilaidoreen@gmail.com>
2026-08-11 13:05:06 +0300

    fix: rename AuthRoutesLayout to RootLayout and adjust sign-in redirect logic

--- Real-estate/app/(root)/_layout.tsx
+++ Real-estate/app/(root)/_layout.tsx
@@ -1,12 +1,15 @@
 import { useAuth} from "@clerk/expo";
 import { Redirect, Slot} from "expo-router";
 
-export default function AuthRoutesLayout() {
+export default function RootLayout() {
   const { isSignedIn, isLoaded} = useAuth()
 
   //sync  clerk user  tto superbase(will build later)
   if (!isLoaded) return null;
-  if (isSignedIn) return <Redirect href="/sign-in"/>
+
+  if (!isSignedIn) {
+    return <Redirect href="/sign-in" />
+  }
 
   return <Slot/>
 }
