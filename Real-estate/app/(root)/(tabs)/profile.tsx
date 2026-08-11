c393bcc9fa9cea8624ba4f1f1eecf84a45e63be8
Ikilia Doreen <ikilaidoreen@gmail.com>
2026-08-11 13:05:28 +0300

    fix: refactor Profile component to include sign-out functionality and improve structure

--- Real-estate/app/(root)/(tabs)/profile.tsx
+++ Real-estate/app/(root)/(tabs)/profile.tsx
@@ -1,14 +1,29 @@
-import { View, Text } from 'react-native'
+import { Text, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { useAuth } from '@clerk/expo';
+import { useRouter } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function profile() {
+export default function Profile() {
+  const { signOut } = useAuth();
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+      router.replace('/sign-in'); 
+    } catch (error) {
+      console.error('Error signing out:', error);
+    }
+  }
   return (
     <SafeAreaView>
-      <View>
-        <Text>profile</Text>
-      </View>
-
+    
+        <Text> Profile</Text>
+         <TouchableOpacity onPress={handleSignOut}>
+          <Text>Sign Out</Text>
+        </TouchableOpacity>``
+     
     </SafeAreaView>
   )
 }
