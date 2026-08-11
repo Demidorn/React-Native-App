01cb30efe4479a1c0957de8246276037a5385573
Ikilia Doreen <ikilaidoreen@gmail.com>
2026-08-11 13:05:44 +0300

    fix: correct component name from 'saved' to 'Saved' for consistency

--- Real-estate/app/(root)/(tabs)/saved.tsx
+++ Real-estate/app/(root)/(tabs)/saved.tsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-export default function saved() {
+export default function Saved() {
   return (
      <SafeAreaView>
       <View>
