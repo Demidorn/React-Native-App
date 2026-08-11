2bc536455fa12dc838f71f25b54283adb5af88fe
Ikilia Doreen <ikilaidoreen@gmail.com>
2026-08-11 13:04:43 +0300

    fix: add missing 'md' props to icons in TabLayout for improved accessibility

--- Real-estate/app/(root)/(tabs)/_layout.jsx
+++ Real-estate/app/(root)/(tabs)/_layout.jsx
@@ -4,24 +4,24 @@ export default function TabLayout() {
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
+        <Icon sf="house.fill" md="home" />
         <Label>Home</Label>
-        <Icon sf="house.fill"  />
       </NativeTabs.Trigger>
-      
+
       <NativeTabs.Trigger name="search">
-        <Icon sf="magnifyingglass"  />
+        <Icon sf="magnifyingglass" md="search" />
         <Label>Search</Label>
       </NativeTabs.Trigger>
 
       {/* create property*/}
 
       <NativeTabs.Trigger name="saved">
-        <Icon sf="heart.fill"  />
+        <Icon sf="heart.fill" md="favorite" />
         <Label>Saved</Label>
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="profile">
-        <Icon sf="person.circle"  />
+        <Icon sf="person.circle" md="account_circle" />
         <Label>Profile</Label>
       </NativeTabs.Trigger>
     </NativeTabs>
