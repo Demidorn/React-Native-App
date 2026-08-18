078e4318192f604b4de3c03ea0421b9768afe65f
Ikilia Doreen <ikilaidoreen@gmail.com>
2026-08-18 16:19:27 +0300

    feat: update ESLint and TypeScript configurations to support path aliases

--- Real-estate/eslint.config.js
+++ Real-estate/eslint.config.js
@@ -6,5 +6,15 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    // Let ESLint resolve TS path aliases (like '@/...') using the TypeScript resolver.
+    // Requires: npm i -D eslint-import-resolver-typescript
+    settings: {
+      'import/resolver': {
+        typescript: {
+          // always try to resolve types under `<root>@types` directory even it doesn't contain source code
+          alwaysTryTypes: true,
+        },
+      },
+    },
   },
 ]);
