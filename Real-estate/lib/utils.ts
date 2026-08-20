6bbc62b0e858e583f29e0c52b8942d17dbacb302
Ikilia Doreen <ikilaidoreen@gmail.com>
2026-08-20 15:23:29 +0300

    feat: add utility function to format price values

---
+++ Real-estate/lib/utils.ts
@@ -0,0 +1,18 @@
+export const formartPrice = (value: number): string => {
+  if (value >= 10000000000) {
+    const b = (value / 10000000000).toFixed(1).replace(/\.0$/, '');
+    return `ugx${b}B`;
+  }
+
+  if (value >= 100000000) {
+    const m = (value / 100000000).toFixed(1).replace(/\.0$/, '');
+    return `ugx${m}M`;
+  }
+
+  if (value >= 100000) {
+    const sh = (value / 100000).toFixed(1).replace(/\.0$/, '');
+    return `ugx${sh}Sh`;
+  }
+
+  return `ugx${value.toLocaleString()}`;
+}
